445a208c655bc16de33adc54eafde92c9af1a439
Dat Truong NM <tnmdat183@gmail.com>
2021-08-17 15:19:52 +0700

    add model credential.cs

---
+++ GoogleCalendar_MVC/Models/Credential.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCalendar_MVC.Models
+{
+    public class Credential
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Type { get; set; }
+        public string JsonValue { get; set; }
+
+        [ForeignKey("UserId")]
+        public IdentityUser User { get; set; }
+    }
+}
