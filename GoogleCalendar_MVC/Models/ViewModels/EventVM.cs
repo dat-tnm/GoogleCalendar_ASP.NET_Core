bd338224c43f3aa9f5036d9b95565aeae87bfbcc
Dat Truong NM <tnmdat183@gmail.com>
2021-08-18 10:18:57 +0700

    change all nullable property to not nullable

--- GoogleCalendar_MVC/Models/ViewModels/EventVM.cs
+++ GoogleCalendar_MVC/Models/ViewModels/EventVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,23 +10,26 @@ namespace GoogleCalendar_MVC.Models.ViewModels
     public class EventVM
     {
         public string Id { get; set; }
+        [Required]
         public string Summary { get; set; }
         public string Description { get; set; }
         public string Location { get; set; }
         public string ColorId { get; set; }
+        [Required]
         public DateTime Start { get; set; }
+        [Required]
         public DateTime End { get; set; }
-        public bool? EndTimeUnspecified { get; set; }
+        public bool EndTimeUnspecified { get; set; }
         public string Transparency { get; set; }
         public string Visibility { get; set; }
-        public int? Sequence { get; set; }
-        public bool? AnyoneCanAddSelf { get; set; }
-        public bool? GuestsCanInviteOthers { get; set; }
-        public bool? GuestsCanSeeOtherGuests { get; set; }
-        public bool? GuestsCanModify { get; set; }
-        public bool? PrivateCopy { get; set; }
-        public bool? Locked { get; set; }
-        public bool? AttendeesOmitted { get; set; }
+        public int Sequence { get; set; }
+        public bool AnyoneCanAddSelf { get; set; }
+        public bool GuestsCanInviteOthers { get; set; }
+        public bool GuestsCanSeeOtherGuests { get; set; }
+        public bool GuestsCanModify { get; set; }
+        public bool PrivateCopy { get; set; }
+        public bool Locked { get; set; }
+        public bool AttendeesOmitted { get; set; }
         public string EventType { get; set; }
 
         public IList<string> Recurrence { get; set; }
