a8145261860d46d4273296b76a3503bd91eef646
Dat Truong NM <tnmdat183@gmail.com>
2021-08-18 11:13:55 +0700

    add CalendarIndexVM.cs

---
+++ GoogleCalendar_MVC/Models/ViewModels/CalendarIndexVM.cs
@@ -0,0 +1,17 @@
+﻿using Google.Apis.Calendar.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCalendar_MVC.Models.ViewModels
+{
+    public class CalendarIndexVM
+    {
+        public IList<Event> Events { get; set; }
+        [Range(0, 12)]
+        public DateTime SelectedMonthYear { get; set; }
+    }
+}
