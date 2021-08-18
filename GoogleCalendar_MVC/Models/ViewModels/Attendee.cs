8adb193ee042495ec3b372f676092999bfa110dd
Dat Truong NM <tnmdat183@gmail.com>
2021-08-18 16:34:16 +0700

    Add error property to Attendee.cs

--- GoogleCalendar_MVC/Models/ViewModels/Attendee.cs
+++ GoogleCalendar_MVC/Models/ViewModels/Attendee.cs
@@ -8,15 +8,9 @@ namespace GoogleCalendar_MVC.Models.ViewModels
 {
     public class Attendee
     {
-        public string Id { get; set; }
         public string Email { get; set; }
-        public string DisplayName { get; set; }
-        public bool? Organizer { get; set; }
-        public bool? Self { get; set; }
-        public bool? Resource { get; set; }
-        public bool? Optional { get; set; }
-        public string ResponseStatus { get; set; }
-        public string Comment { get; set; }
-        public int? AdditionalGuests { get; set; }
+        public bool Resource { get; set; }
+        public bool Optional { get; set; }
+        public string Error { get; set; }
     }
 }
