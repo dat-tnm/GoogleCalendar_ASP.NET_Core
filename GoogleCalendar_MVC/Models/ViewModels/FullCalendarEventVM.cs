a98fd8edaea570a66f74fe79f381126c1423751a
Dat Truong NM <tnmdat183@gmail.com>
2021-08-19 20:44:37 +0700

    add FullCalendarEventVM.cs

---
+++ GoogleCalendar_MVC/Models/ViewModels/FullCalendarEventVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCalendar_MVC.Models.ViewModels
+{
+    public class FullCalendarEventVM
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
