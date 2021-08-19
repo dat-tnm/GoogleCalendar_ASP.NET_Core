5a3e6c8c28bfeaa441097920bcfc4f564def1383
Dat Truong NM <tnmdat183@gmail.com>
2021-08-19 13:42:46 +0700

    add EventModelMapper.cs remove code mapping from controller

--- GoogleCalendar_MVC/Controllers/CalendarAppController.cs
+++ GoogleCalendar_MVC/Controllers/CalendarAppController.cs
@@ -60,70 +60,9 @@ namespace GoogleCalendar_MVC.Controllers
 
         }
 
-        private void MappingViewModelToModel(EventVM viewModel, Event model)
-        {
-            model.Summary = viewModel.Summary;
-            model.Start = new EventDateTime()
-            {
-                DateTime = viewModel.Start,
-                TimeZone = "Asia/Ho_Chi_Minh"
-            };
-            model.End = new EventDateTime()
-            {
-                DateTime = viewModel.End,
-                TimeZone = "Asia/Ho_Chi_Minh"
-            };
-            model.Description = viewModel.Description;
-            model.AttendeesOmitted = viewModel.AttendeesOmitted;
-            model.Transparency = viewModel.Transparency;
-            model.Visibility = viewModel.Visibility;
-            model.GuestsCanInviteOthers = viewModel.GuestsCanInviteOthers;
-            model.GuestsCanModify = viewModel.GuestsCanModify;
-            model.GuestsCanSeeOtherGuests = viewModel.GuestsCanSeeOtherGuests;
-            model.AnyoneCanAddSelf = viewModel.AnyoneCanAddSelf;
-            model.PrivateCopy = viewModel.PrivateCopy;
-            model.Locked = viewModel.Locked;
-            model.Location = viewModel.Location;
-        }
+        
 
-        private void MappingModelToViewModel(Event model, EventVM viewModel)
-        {
-            viewModel.Summary = model.Summary;
-            viewModel.Start = model.Start.DateTime == null ? DateTime.Now : (DateTime)model.Start.DateTime;
-            viewModel.End = model.Start.DateTime == null ? DateTime.Now : (DateTime)model.Start.DateTime;
-            viewModel.Description = model.Description;
-            viewModel.AttendeesOmitted = model.AttendeesOmitted == null ? false : (bool)model.AttendeesOmitted;
-            viewModel.Transparency = model.Transparency;
-            viewModel.Visibility = model.Visibility;
-            viewModel.GuestsCanInviteOthers = model.GuestsCanInviteOthers == null ? true : (bool)model.GuestsCanInviteOthers;
-            viewModel.GuestsCanModify = model.GuestsCanModify == null ? false : (bool)model.GuestsCanModify;
-            viewModel.GuestsCanSeeOtherGuests = model.GuestsCanSeeOtherGuests == null ? true : (bool)model.GuestsCanSeeOtherGuests;
-            viewModel.AnyoneCanAddSelf = model.AnyoneCanAddSelf == null ? false : (bool)model.AnyoneCanAddSelf;
-            viewModel.PrivateCopy = model.PrivateCopy == null ? false : (bool)model.PrivateCopy;
-            viewModel.Locked = model.Locked == null ? false : (bool)model.Locked;
-            viewModel.Location = model.Location;
-
-            viewModel.InitializeProperty();
-            if(model.Attendees != null)
-                foreach (var item in model.Attendees)
-                {
-                    viewModel.Attendees.Add(new Attendee()
-                    {
-                        Email = item.Email,
-                        Optional = item.Optional == true ? true : false
-                    });
-                }
-            if (model.Reminders.UseDefault == false)
-                foreach (var item in model.Reminders.Overrides)
-                {
-                    viewModel.Reminders.Overrides.Add(new Reminder()
-                    {
-                        Method = item.Method,
-                        Minutes = item.Minutes == null ? 0 : (int)item.Minutes
-                    });
-                }
-        }
-
+        
 
 
 
@@ -267,7 +206,7 @@ namespace GoogleCalendar_MVC.Controllers
 
 
             Event body = new Event();
-            MappingViewModelToModel(viewModel, body);
+            new EventModelMapper(body, viewModel).MappingToModel();
 
             if (listAttendees.Count > 0)
             {
@@ -309,7 +248,7 @@ namespace GoogleCalendar_MVC.Controllers
                 return NotFound();
             }
             var viewModel = new EventVM();
-            MappingModelToViewModel(result, viewModel);
+            new EventModelMapper(result, viewModel).MappingToViewModel();
             
             return View(viewModel);
         }
@@ -415,7 +354,7 @@ namespace GoogleCalendar_MVC.Controllers
                 result.Reminders.Overrides = listReminder;
             }
             result.Attendees = listAttendees;
-            MappingViewModelToModel(viewModel, result);
+            new EventModelMapper(result, viewModel).MappingToModel();
 
             var post_request = service.Events.Update(result, "primary", result.Id);
             try
---
+++ GoogleCalendar_MVC/Utilities/EventModelMapper.cs
@@ -0,0 +1,86 @@
+﻿using Google.Apis.Calendar.v3.Data;
+using GoogleCalendar_MVC.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCalendar_MVC.Utilities
+{
+    public class EventModelMapper
+    {
+        public Event Model { get; set; }
+        public EventVM ViewModel { get; set; }
+
+        public EventModelMapper(Event model, EventVM viewModel)
+        {
+            Model = model;
+            ViewModel = viewModel;
+        }
+
+        public void MappingToModel()
+        {
+            Model.Summary = ViewModel.Summary;
+            Model.Start = new EventDateTime()
+            {
+                DateTime = ViewModel.Start,
+                TimeZone = "Asia/Ho_Chi_Minh"
+            };
+            Model.End = new EventDateTime()
+            {
+                DateTime = ViewModel.End,
+                TimeZone = "Asia/Ho_Chi_Minh"
+            };
+            Model.Description = ViewModel.Description;
+            Model.AttendeesOmitted = ViewModel.AttendeesOmitted;
+            Model.Transparency = ViewModel.Transparency;
+            Model.Visibility = ViewModel.Visibility;
+            Model.GuestsCanInviteOthers = ViewModel.GuestsCanInviteOthers;
+            Model.GuestsCanModify = ViewModel.GuestsCanModify;
+            Model.GuestsCanSeeOtherGuests = ViewModel.GuestsCanSeeOtherGuests;
+            Model.AnyoneCanAddSelf = ViewModel.AnyoneCanAddSelf;
+            Model.PrivateCopy = ViewModel.PrivateCopy;
+            Model.Locked = ViewModel.Locked;
+            Model.Location = ViewModel.Location;
+        }
+
+        public void MappingToViewModel()
+        {
+            ViewModel.Summary = Model.Summary;
+            ViewModel.Start = Model.Start.DateTime == null ? DateTime.Now : (DateTime)Model.Start.DateTime;
+            ViewModel.End = Model.Start.DateTime == null ? DateTime.Now : (DateTime)Model.Start.DateTime;
+            ViewModel.Description = Model.Description;
+            ViewModel.AttendeesOmitted = Model.AttendeesOmitted == null ? false : (bool)Model.AttendeesOmitted;
+            ViewModel.Transparency = Model.Transparency;
+            ViewModel.Visibility = Model.Visibility;
+            ViewModel.GuestsCanInviteOthers = Model.GuestsCanInviteOthers == null ? true : (bool)Model.GuestsCanInviteOthers;
+            ViewModel.GuestsCanModify = Model.GuestsCanModify == null ? false : (bool)Model.GuestsCanModify;
+            ViewModel.GuestsCanSeeOtherGuests = Model.GuestsCanSeeOtherGuests == null ? true : (bool)Model.GuestsCanSeeOtherGuests;
+            ViewModel.AnyoneCanAddSelf = Model.AnyoneCanAddSelf == null ? false : (bool)Model.AnyoneCanAddSelf;
+            ViewModel.PrivateCopy = Model.PrivateCopy == null ? false : (bool)Model.PrivateCopy;
+            ViewModel.Locked = Model.Locked == null ? false : (bool)Model.Locked;
+            ViewModel.Location = Model.Location;
+
+            ViewModel.InitializeProperty();
+            if (Model.Attendees != null)
+                foreach (var item in Model.Attendees)
+                {
+                    ViewModel.Attendees.Add(new Attendee()
+                    {
+                        Email = item.Email,
+                        Optional = item.Optional == true ? true : false
+                    });
+                }
+            if (Model.Reminders.UseDefault == false)
+                foreach (var item in Model.Reminders.Overrides)
+                {
+                    ViewModel.Reminders.Overrides.Add(new Reminder()
+                    {
+                        Method = item.Method,
+                        Minutes = item.Minutes == null ? 0 : (int)item.Minutes
+                    });
+                }
+        }
+    }
+}
