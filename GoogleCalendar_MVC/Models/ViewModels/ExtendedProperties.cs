7bf99619119c6256df1d350119d2c7e5172bee5f
Dat Truong NM <tnmdat183@gmail.com>
2021-08-19 08:44:51 +0700

    remove virtual keyword

--- GoogleCalendar_MVC/Models/ViewModels/ExtendedProperties.cs
+++ GoogleCalendar_MVC/Models/ViewModels/ExtendedProperties.cs
@@ -11,10 +11,10 @@ namespace GoogleCalendar_MVC.Models.ViewModels
         //
         // Summary:
         //     Properties that are private to the copy of the event that appears on this calendar.
-        public virtual IDictionary<string, string> Private__ { get; set; }
+        public IDictionary<string, string> Private__ { get; set; }
         //
         // Summary:
         //     Properties that are shared between copies of the event on other attendees' calendars.
-        public virtual IDictionary<string, string> Shared { get; set; }
+        public IDictionary<string, string> Shared { get; set; }
     }
 }
