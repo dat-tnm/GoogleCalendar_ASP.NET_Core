68190df17af3539d492c7a7a6c1d9a96f832fdbf
Dat Truong NM <tnmdat183@gmail.com>
2021-08-19 08:46:23 +0700

    remove nullable ? keyword

--- GoogleCalendar_MVC/Models/ViewModels/Reminders.cs
+++ GoogleCalendar_MVC/Models/ViewModels/Reminders.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +11,14 @@ namespace GoogleCalendar_MVC.Models.ViewModels
     public class Reminders
     {
         public IList<Reminder> Overrides { get; set; }
-        public bool? UseDefault { get; set; }
+        public bool UseDefault { get; set; }
     }
 
     public class Reminder
     {
         public string Method { get; set; }
-        public int? Minutes { get; set; }
+        [Range(0, 40200)]
+        public int Minutes { get; set; }
+        public string Error { get; set; }
     }
 }
