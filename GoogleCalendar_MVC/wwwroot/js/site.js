ae998e094eb0224cf45f52875aae3d1e137b00a3
Dat Truong NM <tnmdat183@gmail.com>
2021-08-20 15:49:21 +0700

    add function DeleteWithoutReload(url)

--- GoogleCalendar_MVC/wwwroot/js/site.js
+++ GoogleCalendar_MVC/wwwroot/js/site.js
@@ -23,3 +23,28 @@
         }
     });
 }
+
+function DeleteWithoutReload(url) {
+    swal({
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: 'DELETE',
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
