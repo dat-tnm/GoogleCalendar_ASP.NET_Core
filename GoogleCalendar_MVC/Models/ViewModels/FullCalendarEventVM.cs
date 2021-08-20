2dceab91ae06d187b1339db93af526bfca572354
Dat Truong NM <tnmdat183@gmail.com>
2021-08-20 11:36:47 +0700

    add construtor to convert from ExpandoObject

--- GoogleCalendar_MVC/Models/ViewModels/FullCalendarEventVM.cs
+++ GoogleCalendar_MVC/Models/ViewModels/FullCalendarEventVM.cs
@@ -1,16 +1,37 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GoogleCalendar_MVC.Models.ViewModels
 {
     public class FullCalendarEventVM
     {
+        public FullCalendarEventVM()
+        {
+
+        }
+
+        public FullCalendarEventVM(IDictionary<string, object> jsonObj)
+        {
+            if (jsonObj.ContainsKey("id"))
+                Id = jsonObj["id"].ToString();
+            if (jsonObj.ContainsKey("title"))
+                Title = jsonObj["title"].ToString();
+            if (jsonObj.ContainsKey("start"))
+                Start = Convert.ToDateTime(jsonObj["start"].ToString());
+            if (jsonObj.ContainsKey("end"))
+                End = Convert.ToDateTime(jsonObj["end"].ToString());
+        }
+
+        [JsonProperty("id")]
         public string Id { get; set; }
+        [JsonProperty("title")]
         public string Title { get; set; }
+        [JsonProperty("start")]
         public DateTime Start { get; set; }
+        [JsonProperty("end")]
         public DateTime End { get; set; }
     }
 }
