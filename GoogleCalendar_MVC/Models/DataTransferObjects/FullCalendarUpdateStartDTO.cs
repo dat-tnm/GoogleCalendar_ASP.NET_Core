b819e18e5bdfa5d6d245a0a995ff3607ce0ed08a
Dat Truong NM <tnmdat183@gmail.com>
2021-08-20 12:01:00 +0700

    add FullCalendarUpdateStartDTO.cs

---
+++ GoogleCalendar_MVC/Models/DataTransferObjects/FullCalendarUpdateStartDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCalendar_MVC.Models.DataTransferObjects
+{
+    public class FullCalendarUpdateStartDTO
+    {
+        public string Id { get; set; }
+        public DateTime Start { get; set; }
+        public int Minutes  { get; set; }
+
+        public FullCalendarUpdateStartDTO(IDictionary<string, object> jsonObj)
+        {
+            if (jsonObj.ContainsKey("id"))
+                Id = jsonObj["id"].ToString();
+            if (jsonObj.ContainsKey("minutes"))
+                Minutes = int.Parse(jsonObj["minutes"].ToString());
+            if (jsonObj.ContainsKey("start"))
+                Start = Convert.ToDateTime(jsonObj["start"].ToString());
+        }
+    }
+}
