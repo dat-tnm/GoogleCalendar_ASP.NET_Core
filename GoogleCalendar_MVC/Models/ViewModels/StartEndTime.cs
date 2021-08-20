0dc504b6a19263417832e6f36fc16269794fb5a5
Dat Truong NM <tnmdat183@gmail.com>
2021-08-20 11:35:47 +0700

    add StartEndTime view model

---
+++ GoogleCalendar_MVC/Models/ViewModels/StartEndTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleCalendar_MVC.Models.ViewModels
+{
+    public class StartEndTime
+    {
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
